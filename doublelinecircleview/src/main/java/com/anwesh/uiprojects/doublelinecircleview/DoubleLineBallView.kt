d7e374aae9d0c4286313bdbf999a870f5394f47f
Anwesh43 <anweshthecool0@gmail.com>
2019-01-21 23:20:00 +0530

    adding drawDLBNode canvas extension method

--- doublelinecircleview/src/main/java/com/anwesh/uiprojects/doublelinecircleview/DoubleLineBallView.kt
+++ doublelinecircleview/src/main/java/com/anwesh/uiprojects/doublelinecircleview/DoubleLineBallView.kt
@@ -17,6 +17,7 @@ val circles : Int = 2
 val scGap : Float = 0.05f
 val scDiv : Double = 0.51
 val strokeFactor : Int = 90
+val rFactor : Int = 4
 val sizeFactor : Float = 2.8f
 val foreColor : Int = Color.parseColor("#1565C0")
 val backColor : Int = Color.parseColor("#BDBDBD")
@@ -27,3 +28,29 @@ fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawDLBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val r : Float = size / 4
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    save()
+    translate(w/2, gap * (i + 1))
+    rotate(90f * (1 - sc1))
+    drawLine(0f, -size, 0f, size, paint)
+    for (j in 0..(circles - 1)) {
+        val sf : Float = 1f - 2 * (j % 2)
+        val sc : Float = sc2.divideScale(j, circles)
+        save()
+        translate((w/2 + r) * sc * sf, (size - r) * sf)
+        drawCircle(0f, 0f, r, paint)
+        restore()
+    }
+    restore()
+}
